3c7cf6741d7068b92fa0ceeb45112571511040de
gabrielpinacio <gabrielpinacio@protonmail.com>
2021-11-13 18:23:59 -0300

    habilitando manipulação da planilha

--- src/spreadsheets/views.py
+++ src/spreadsheets/views.py
@@ -3,6 +3,7 @@ import pandas as pd
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from .models import Spreadsheet
+import openpyxl as xl
 
 # Create your views here.
 def home_view(request):
@@ -17,11 +18,12 @@ def file_upload_view(request):
     
     if request.method == 'POST':
         xsl_file = request.FILES.get('file')
-        obj = Spreadsheet.objects.create(file_name=xsl_file)
+        obj = Spreadsheet.objects.create(file_name=xsl_file, xsl_file= xsl_file)
         obj.save()
-    """
-        with open(obj.file_name.path, 'r') as f:
-            reader = pd.read_excel(f) 
-            for row in reader:
-               print(row, type(row)) """
+        #f = obj.file.open('r')
+        #with open(obj.file_name, 'r') as f:
+        #f = obj.file_name.open('r')
+        reader = pd.read_excel(xsl_file)
+        for row in reader:
+            print(row, type(row))
     return HttpResponse()
